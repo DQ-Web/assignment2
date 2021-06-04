d02bb4506446997a4186c3c90afb90084382e260
DavidUllauri <ullauridavid@gmail.com>
2021-06-04 16:27:36 -0400

    Implemented myMap

--- myMap.js
+++ myMap.js
@@ -3,5 +3,27 @@ map()
 
 Without using the native “Array.prototype.map” method of JavaScript, compose a function titled “myMap” that will take in an array of elements and execute a callback function on each of those elements.
 */
+if (!Array.prototype.myMap) {
+    Array.prototype.myMap = function (callback) {
+        let newArr = [];
+        for (let i=0; i<this.length; i++) {
+            let mapItem = callback(this[i], i, this);
+            newArr.push(mapItem);
+        }
+        return newArr;
+    }
+}
 
 /* TEST */
+const arr1 = [1, 2, 3, 4];
+const mapOg = arr1.map(x => x * 2);
+const map1 = arr1.myMap(x => x * 2);
+
+console.log("Array-----");
+console.log(arr1);
+
+console.log("Map-------");
+console.log(mapOg);
+
+console.log("My Map----");
+console.log(map1);
