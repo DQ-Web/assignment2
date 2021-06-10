af2140e2bb843fa80f3d65e5bf770a9a6d0dbd70
Quymbee Chen <quymbee@mattchen.com>
2021-06-10 18:55:36 -0400

    finished grabKeys()"

--- grabKeys.js
+++ grabKeys.js
@@ -4,4 +4,23 @@ Object.keys()
 Without using the native “Object.keys()” method of JavaScript, compose a function titled “grabKeys” that will take in an object and return all of the keys of the key:value pairs of that object.
 */
 
+Object.prototype.grabKeys = function (obj) {
+  if (typeof obj !== "object" || obj === null)
+    throw new TypeError("Object.grabKeys called on non-object");
+
+  let newArr = [];
+
+  for (key in obj) {
+    obj.hasOwnProperty(key) && newArr.push(key);
+  }
+  return newArr;
+};
+
 /* TEST */
+const obj = { a: "somestring", b: 42, c: 0 };
+
+console.log("Object.keys()");
+console.log(Object.keys(obj));
+
+console.log("Object.grabKeys()");
+console.log(Object.grabKeys(obj));
