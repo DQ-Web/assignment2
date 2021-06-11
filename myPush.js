7fed4b36800fc7f0baf3c0a2cf12462860c69d48
Quymbee Chen <quymbee@mattchen.com>
2021-06-10 20:02:27 -0400

    implement a test file

--- myPush.js
+++ myPush.js
@@ -4,8 +4,6 @@ push()
 Without using the native “Array.prototype.push” method of JavaScript, compose a function titled “myPush” that will take in an array of elements as well as an elementToAdd and append that element to the end of the array.
 */
 
-const arr = [1, 2, 3];
-
 Array.prototype.myPush = function (...args) {
   let arg_i = 0;
   let length = this.length;
@@ -17,6 +15,14 @@ Array.prototype.myPush = function (...args) {
   return this.length;
 };
 
+const arr = [1, 2, 3];
+const arr2 = [1, 2, 3];
+
 /* TEST */
+arr2.push(4, 5, 6);
+console.log("push()");
+console.log(arr2);
+
 arr.myPush(4, 5, 6);
-console.log("myPush: ", arr);
+console.log("myPush()");
+console.log(arr);
